d10f236671700953c26b3eff2b3274e2ab721349
moroviintaas <moroviintaas@gmail.com>
2022-09-22 21:43:15 +0200

    Cosmetics.

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ use brydz_core::player::side::{Side, SideAssociated};
 use brydz_core::player::situation::Situation;
 use brydz_core::protocol::{ClientDealMessage, ServerDealMessage};
 use brydz_core::world::agent::{AutomaticAgent, DummyBot};
-use brydz_core::world::comm::SyncComm;
+use brydz_core::world::comm::{SyncComm, TokioComm};
 use brydz_core::world::environment::{ NoCardCheck, RoundRobinDealEnvironment};
 use karty::cards::CardStd;
 use karty::speedy::{Writable, Readable};
@@ -38,6 +38,7 @@ fn setup_logger() -> Result<(), fern::InitError> {
     Ok(())
 }
 
+#[allow(dead_code)]
 fn basic_sim_with_bot3(){
     let contract = Contract::new(Side::East, Bid::init(Trump::Colored(Spades), 2).unwrap());
     let deal = RegDealStd::new(contract.clone());
@@ -63,10 +64,74 @@ fn basic_sim_with_bot3(){
     //let (w_tx, w_rx) = comm_west._decompose();
 
     let mut card_supply = Vec::from(STANDARD_DECK);
-    let hand_east = BridgeHand::init(&mut card_supply).unwrap();
-    let hand_south = BridgeHand::init(&mut card_supply).unwrap();
-    let hand_west = BridgeHand::init(&mut card_supply).unwrap();
-    let hand_north = BridgeHand::init(&mut card_supply).unwrap();
+    let hand_east = BridgeHand::drain_full_from_vec(&mut card_supply).unwrap();
+    let hand_south = BridgeHand::drain_full_from_vec(&mut card_supply).unwrap();
+    let hand_west = BridgeHand::drain_full_from_vec(&mut card_supply).unwrap();
+    let hand_north = BridgeHand::drain_full_from_vec(&mut card_supply).unwrap();
+
+    //let mut bot_east = brydz_bot_random::declarer::DeclarerOverChannel::new(e_tx, e_rx, Situation::new(Side::East, hand_east, contract.clone()));
+
+    let mut bot_east = brydz_bot_random::declarer::DeclarerBot::new(comm_east, Situation::new(Side::East, hand_east, contract.clone()));
+    //let mut bot_south = brydz_bot_random::defender::DefenderOverChannel::new(s_tx, s_rx, Situation::new(Side::South, hand_south, contract.clone()));
+    let mut bot_south = brydz_bot_random::defender::DefenderBot::new(comm_south, Situation::new(Side::South, hand_south, contract.clone()));
+    let mut bot_west = DummyBot::new(comm_west, Situation::new(Side::West, hand_west, contract.clone()));
+    //let mut bot_north = brydz_bot_random::defender::DefenderOverChannel::new(n_tx, n_rx, Situation::new(Side::North, hand_north, contract));
+    let mut bot_north = brydz_bot_random::defender::DefenderBot::new(comm_north, Situation::new(Side::North, hand_north, contract));
+
+
+
+    thread::scope(|s|{
+        s.spawn(||{
+           simple_overseer.run().unwrap();
+            //println!("{:?}", x);
+        });
+        s.spawn(||{
+            bot_east.run().unwrap();
+        });
+        s.spawn(||{
+            bot_south.run().unwrap();
+            //error!("South result: {:?}", &rs);
+        });
+        s.spawn(||{
+            bot_west.run().unwrap();
+        });
+        s.spawn(||{
+            bot_north.run().unwrap();
+        });
+    })
+
+
+}
+
+fn basic_sim_with_bot_tokio(){
+    let contract = Contract::new(Side::East, Bid::init(Trump::Colored(Spades), 2).unwrap());
+    let deal = RegDealStd::new(contract.clone());
+    //let mut simple_overseer = SimpleOverseer::new(deal);
+    let (comm_env_north, comm_north) = TokioComm::<ServerDealMessage, ClientDealMessage, BridgeErrorStd>::new_pair();
+    let (comm_env_east, comm_east) = TokioComm::<ServerDealMessage, ClientDealMessage, BridgeErrorStd>::new_pair();
+    let (comm_env_west, comm_west) = TokioComm::<ServerDealMessage, ClientDealMessage, BridgeErrorStd>::new_pair();
+    let (comm_env_south, comm_south) = TokioComm::<ServerDealMessage, ClientDealMessage, BridgeErrorStd>::new_pair();
+
+    let comm_assotiation = SideAssociated::new(comm_env_north, comm_env_east, comm_env_south, comm_env_west);
+
+
+    let mut simple_overseer = RoundRobinDealEnvironment::new(comm_assotiation, deal, NoCardCheck::default());
+    /*
+    let (n_tx, n_rx) = simple_overseer.create_connection(&North);
+    let (s_tx, s_rx) = simple_overseer.create_connection(&South);
+    let (e_tx, e_rx) = simple_overseer.create_connection(&East);
+    let (w_tx, w_rx) = simple_overseer.create_connection(&West);
+    */
+    //let (n_tx, n_rx) = comm_north._decompose();
+    //let (s_tx, s_rx) = comm_south._decompose();
+    //let (e_tx, e_rx) = comm_east._decompose();
+    //let (w_tx, w_rx) = comm_west._decompose();
+
+    let mut card_supply = Vec::from(STANDARD_DECK);
+    let hand_east = BridgeHand::drain_full_from_vec(&mut card_supply).unwrap();
+    let hand_south = BridgeHand::drain_full_from_vec(&mut card_supply).unwrap();
+    let hand_west = BridgeHand::drain_full_from_vec(&mut card_supply).unwrap();
+    let hand_north = BridgeHand::drain_full_from_vec(&mut card_supply).unwrap();
 
     //let mut bot_east = brydz_bot_random::declarer::DeclarerOverChannel::new(e_tx, e_rx, Situation::new(Side::East, hand_east, contract.clone()));
 
@@ -182,7 +247,8 @@ fn main(){
     
     println!("Hello!");
     //basic_sim_with_bot2();
-    basic_sim_with_bot3();
+    //basic_sim_with_bot3();
+    basic_sim_with_bot_tokio();
     //let rt = tokio::runtime::Runtime::new().unwrap();
     //rt.block_on(async {basic_sim_with_bot_tcp().await});
     //test_tcp();
