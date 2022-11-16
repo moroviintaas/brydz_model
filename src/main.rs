00fb05d05858d19370ec9e5d8d618ac0ed99383e
moroviintaas <moroviintaas@gmail.com>
2022-11-16 09:45:28 +0100

    Tcp fixed after changing framework.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 
 use std::thread;
+use log::info;
 use brydz_framework::brydz_core::deal::fair_bridge_deal;
 use brydz_framework::error::comm::CommError;
 use brydz_framework::world::agent::bot::{SimpleRandomBot2PhaseStd, DummyBotPhase2Std};
@@ -12,9 +13,11 @@ use brydz_framework::brydz_core::cards::trump::Trump;
 use brydz_framework::brydz_core::contract::{ContractSpec, Contract};
 use brydz_framework::brydz_core::karty::suits::Suit::Spades;
 use brydz_framework::brydz_core::player::side::{Side, SideAssociated};
+use brydz_framework::error::{BridgeError, BridgeErrorGen};
 use brydz_framework::protocol::{ServerDealMessageStd, ClientDealMessageStd};
 use brydz_framework::world::agent::{ AutomaticAgentPhase2};
 use brydz_framework::world::comm::{SyncComm};
+use brydz_network_extensions::tcp::speedy::TcpComm;
 use karty::hand::StackHand;
 
 fn setup_logger() -> Result<(), fern::InitError> {
@@ -174,10 +177,10 @@ fn test_std_tcp(){
 fn basic_sim_with_bot(){
     let contract = ContractSpec::new(Side::East, Bid::init(Trump::Colored(Spades), 2).unwrap());
     //let mut simple_overseer = SimpleOverseer::new(contract);
-    let (comm_env_north, comm_north) = SyncComm::<ServerDealMessageStd, ClientDealMessageStd, CommError>::new_pair();
-    let (comm_env_east, comm_east) = SyncComm::<ServerDealMessageStd, ClientDealMessageStd, CommError>::new_pair();
-    let (comm_env_west, comm_west) = SyncComm::<ServerDealMessageStd, ClientDealMessageStd, CommError>::new_pair();
-    let (comm_env_south, comm_south) = SyncComm::<ServerDealMessageStd, ClientDealMessageStd, CommError>::new_pair();
+    let (comm_env_north, comm_north) = SyncComm::<ServerDealMessageStd, ClientDealMessageStd, BridgeError>::new_pair();
+    let (comm_env_east, comm_east) = SyncComm::<ServerDealMessageStd, ClientDealMessageStd, BridgeError>::new_pair();
+    let (comm_env_west, comm_west) = SyncComm::<ServerDealMessageStd, ClientDealMessageStd, BridgeError>::new_pair();
+    let (comm_env_south, comm_south) = SyncComm::<ServerDealMessageStd, ClientDealMessageStd, BridgeError>::new_pair();
 
     let comm_assotiation = SideAssociated::new(comm_env_north, comm_env_east, comm_env_south, comm_env_west);
 
@@ -187,16 +190,7 @@ fn basic_sim_with_bot(){
         comm_assotiation,
         EnvStatePhase2::new(initial_contract.clone()));
 
-    //let (n_tx, n_rx) = comm_north._decompose();
-    //let (s_tx, s_rx) = comm_south._decompose();
-    //let (e_tx, e_rx) = comm_east._decompose();
-    //let (w_tx, w_rx) = comm_west._decompose();
 
-    //let mut card_supply = Vec::from(STANDARD_DECK);
-    //let hand_east = HandVector::drain_full_from_vec(&mut card_supply).unwrap();
-    //let hand_south = HandVector::drain_full_from_vec(&mut card_supply).unwrap();
-    //let hand_west = HandVector::drain_full_from_vec(&mut card_supply).unwrap();
-    //let hand_north = HandVector::drain_full_from_vec(&mut card_supply).unwrap();
 
     let card_deal = fair_bridge_deal::<StackHand>();
     let (hand_north, hand_east, hand_south, hand_west) = card_deal.destruct();
@@ -257,9 +251,101 @@ fn basic_sim_with_bot(){
 
 }
 
+fn basic_sim_with_bot_tcp(){
+    let contract = ContractSpec::new(Side::East, Bid::init(Trump::Colored(Spades), 2).unwrap());
+    //let mut simple_overseer = SimpleOverseer::new(contract);
+    let tcp_listener = std::net::TcpListener::bind("127.0.0.1:8420").unwrap();
+
+    let initial_contract = Contract::new(contract);
+    let initial_contract_env = initial_contract.clone();
+
+
+    thread::scope(|s|{
+
+        s.spawn(||{
+            let (north_stream, _) = tcp_listener.accept().unwrap();
+            info!("North connected");
+            let (east_stream, _) = tcp_listener.accept().unwrap();
+            info!("East connected");
+            let (south_stream, _) = tcp_listener.accept().unwrap();
+            info!("South connected");
+            let (west_stream, _) = tcp_listener.accept().unwrap();
+            info!("West connected");
+            let comm_assotiation = SideAssociated::new(TcpComm::new(north_stream), TcpComm::new(east_stream), TcpComm::new(south_stream), TcpComm::new(west_stream));
+            let simple_overseer = RoundRobinContractEnvStd::new(
+            comm_assotiation,
+                EnvStatePhase2::new(initial_contract_env.clone()));
+            //let mut simple_overseer = RoundRobinDealEnvironment::new(comm_assotiation, deal, NoCardCheck::default());
+
+
+            simple_overseer.run().unwrap();
+        });
+
+        s.spawn(||{
+            let stream_north_c = std::net::TcpStream::connect("127.0.0.1:8420").unwrap();
+            info!("North connected (client)");
+            let stream_east_c = std::net::TcpStream::connect("127.0.0.1:8420").unwrap();
+            info!("East connected (client)");
+            let stream_south_c = std::net::TcpStream::connect("127.0.0.1:8420").unwrap();
+            info!("South connected (client)");
+            let stream_west_c = std::net::TcpStream::connect("127.0.0.1:8420").unwrap();
+            info!("West connected (client)");
+
+            let comm_north = TcpComm::new(stream_north_c);
+            let comm_east = TcpComm::new(stream_east_c);
+            let comm_south = TcpComm::new(stream_south_c);
+            let comm_west = TcpComm::new(stream_west_c);
+            
+            let card_deal = fair_bridge_deal::<StackHand>();
+            let (hand_north, hand_east, hand_south, hand_west) = card_deal.destruct();
+
+            let mut bot_east = SimpleRandomBot2PhaseStd::new(AgentStatePhase2::new(
+                Side::East,
+                hand_east,
+                initial_contract.clone()), comm_east);
+            let mut bot_west = DummyBotPhase2Std::new(AgentStatePhase2::new(
+                Side::West,
+                hand_west,
+                initial_contract.clone()), comm_west);
+            let mut bot_north = SimpleRandomBot2PhaseStd::new(AgentStatePhase2::new(
+                Side::North,
+                hand_north,
+                initial_contract.clone()), comm_north);
+            let mut bot_south = SimpleRandomBot2PhaseStd::new(AgentStatePhase2::new(
+                Side::South,
+                hand_south,
+                initial_contract), comm_south);
+
+            thread::scope(|s2|{
+
+                s2.spawn(||{
+                    bot_north.run().unwrap();
+                });
+                s2.spawn(||{
+                    bot_south.run().unwrap();
+                });
+                s2.spawn(||{
+                    bot_west.run().unwrap();
+                });
+                s2.spawn(||{
+                    bot_east.run().unwrap();
+                });
+            });
+
+
+
+        });
+
+    });
+
+
+
+}
+
 fn main(){
     setup_logger().unwrap();
     
     println!("Hello!");
     basic_sim_with_bot();
+    basic_sim_with_bot_tcp();
 }
