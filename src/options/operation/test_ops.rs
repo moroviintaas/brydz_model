5e88ab60501be6d9019ca678aeb4601c0da0fb6b
moroviintaas <moroviintaas@gmail.com>
2023-04-24 17:00:55 +0200

    Play contract with mixed Comm on agents.

--- src/options/operation/test_ops.rs
+++ src/options/operation/test_ops.rs
@@ -1,4 +1,4 @@
-use std::thread;
+use std::thread::{self, spawn};
 use log::info;
 use brydz_core::bidding::Bid;
 use brydz_core::cards::trump::TrumpGen;
@@ -16,7 +16,8 @@ use karty::suits::Suit::{Spades};
 use sztorm::automatons::rr::{AgentAuto, EnvironmentRR, RoundRobinModelBuilder};
 use sztorm::error::{CommError, SztormError};
 use sztorm::protocol::{AgentMessage, EnvMessage};
-use sztorm_net_ext::tcp::TcpCommK1;
+use sztorm_net_ext::{ComplexComm, ComplexComm2048};
+use sztorm_net_ext::tcp::{TcpCommK1, TcpCommK2};
 use sztorm::{AgentGen, RandomPolicy};
 use brydz_core::sztorm::state::ContractState;
 use sztorm::EnvironmentState;
@@ -164,12 +165,40 @@ pub fn tur_sim_tcp(){
 }
 
 pub fn test_generic_model() -> Result<(), SztormError<ContractProtocolSpec>>{
+    type TcpCommSim = TcpCommK2<AgentMessage<ContractProtocolSpec>, EnvMessage<ContractProtocolSpec>, CommError<ContractProtocolSpec>>;
+    type TcpCommSimEnv = TcpCommK2<EnvMessage<ContractProtocolSpec>, AgentMessage<ContractProtocolSpec>, CommError<ContractProtocolSpec>>;
     let contract_params = ContractParametersGen::new(Side::East, Bid::init(TrumpGen::Colored(Spades), 2).unwrap());
     let (comm_env_north, comm_north) = ContractEnvSyncComm::new_pair();
+    
     let (comm_env_east, comm_east) = ContractEnvSyncComm::new_pair();
     let (comm_env_west, comm_west) = ContractEnvSyncComm::new_pair();
     let (comm_env_south, comm_south) = ContractEnvSyncComm::new_pair();
 
+    let tcp_listener = std::net::TcpListener::bind("127.0.0.1:8420").unwrap();
+    let (t, r) = std::sync::mpsc::channel();
+
+    thread::spawn(move ||{
+        let (south_stream_env_side, _) = tcp_listener.accept().unwrap();
+        t.send(south_stream_env_side).unwrap();
+    } );
+
+    let stream_south_agent_side = std::net::TcpStream::connect("127.0.0.1:8420").unwrap();
+    let south_stream_env_side = r.recv().unwrap();
+    let env_comm_south = ComplexComm::Tcp(TcpCommSimEnv::new(south_stream_env_side)) ;
+
+    let agent_comm_south = ComplexComm::Tcp(TcpCommSim::new(stream_south_agent_side));
+
+/*
+    let comm_env_north = ComplexComm2048::StdSync(comm_env_north);
+    let comm_env_east = ComplexComm2048::StdSync(comm_env_east);
+    let comm_env_south = ComplexComm2048::StdSync(comm_env_south);
+    let comm_env_west = ComplexComm2048::StdSync(comm_env_west);
+
+    let comm_north = ComplexComm2048::StdSync(comm_north);
+    let comm_east = ComplexComm2048::StdSync(comm_east);
+    let comm_south = ComplexComm2048::StdSync(comm_south);
+    let comm_west = ComplexComm2048::StdSync(comm_west);
+*/
     let card_deal = fair_bridge_deal::<CardSet>();
     let (hand_north, hand_east, hand_south, hand_west) = card_deal.destruct();
     let initial_contract = Contract::new(contract_params);
@@ -183,7 +212,7 @@ pub fn test_generic_model() -> Result<(), SztormError<ContractProtocolSpec>>{
     let policy_dummy = RandomPolicy::<ContractProtocolSpec, ContractDummyState>::new();
 
     let mut agent_east = AgentGen::new(East, initial_state_east, comm_east, random_policy.clone() );
-    let mut agent_south = AgentGen::new(South, initial_state_south, comm_south, random_policy.clone() );
+    let mut agent_south = AgentGen::new(South, initial_state_south, agent_comm_south, random_policy.clone() );
     let mut agent_west = AgentGen::new(West, initial_state_west, comm_west, policy_dummy);
     let mut agent_north = AgentGen::new(North, initial_state_north, comm_north, random_policy );
 
@@ -200,12 +229,21 @@ pub fn test_generic_model() -> Result<(), SztormError<ContractProtocolSpec>>{
             state.update(state_update)?;
             Ok(vec![(North,state_update),(East,state_update),(South,state_update), (West, state_update)])
         })?
-        .with_local_agent(Box::new(agent_east), Box::new(comm_env_east))?
-        .with_local_agent(Box::new(agent_south), Box::new(comm_env_south))?
-        .with_local_agent(Box::new(agent_west), Box::new(comm_env_west))?
-        .with_local_agent(Box::new(agent_north), Box::new(comm_env_north))?
+        .with_local_agent(Box::new(agent_east), ComplexComm2048::StdSync(comm_env_east))?
+        //.with_local_agent(Box::new(agent_south), agent_comm_south)?
+        .with_local_agent(Box::new(agent_west), ComplexComm2048::StdSync(comm_env_west))?
+        .with_local_agent(Box::new(agent_north), ComplexComm2048::StdSync(comm_env_north))?
+        .with_remote_agent(Side::South, env_comm_south)?
         .build()?;
 
+
+
+    
+
+    thread::spawn(move || {
+        agent_south.run_rr().unwrap();
+    });
     model.play().unwrap();
+
     Ok(())
 }
