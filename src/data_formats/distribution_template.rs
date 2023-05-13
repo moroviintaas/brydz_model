15494e03c7c67a52eeff229ed12513cf58639a94
moroviintaas <moroviintaas@gmail.com>
2023-05-13 13:39:16 +0200

    backing changes during work on fuzzysets

--- src/data_formats/distribution_template.rs
+++ src/data_formats/distribution_template.rs
@@ -1,10 +1,10 @@
 use serde::{Deserialize, Serialize};
-use brydz_core::sztorm::state::{ HandInfoSuspect};
+use brydz_core::sztorm::state::{CardDistribution};
 
 
 #[derive(Deserialize, Serialize, Clone)]
 #[allow(clippy::large_enum_variant)]
 pub enum DistributionTemplate {
     Simple,
-    Suspect(HandInfoSuspect)
+    Suspect(CardDistribution)
 }
